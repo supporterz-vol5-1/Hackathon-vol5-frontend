6e5430b617ad7d823c509cefa9a6404ab687e51e
keimoriyama <keischwiiz@gmail.com>
2021-07-04 12:44:54 +0900

    ユーザー名とユーザー名に対する操作を決めれるようにした

--- app/src/Navibar.js
+++ app/src/Navibar.js
@@ -3,15 +3,26 @@ import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
 import { Form, FormControl } from "react-bootstrap";
 
+const options = { register: 1, "change account": 2 };
 class Navibar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
+      option: 1,
     };
   }
   handleinput = (e) => {
     this.setState({ username: e.target.value });
+  };
+
+  handletoggleinput = (e) => {
+    console.log(options[e.target.value]);
+    this.setState({ option: options[e.target.value] });
+  };
+
+  push_data = () => {
+    console.log(this.state);
   };
   render() {
     return (
@@ -25,11 +36,11 @@ class Navibar extends React.Component {
             className="mr-sm-2"
             onChange={this.handleinput}
           />
-          <Form.Control as="select" size="sm">
+          <Form.Control as="select" size="sm" onChange={this.handletoggleinput}>
             <option>register</option>
             <option>change account</option>
           </Form.Control>
-          <Button variant="secondary" type="submit">
+          <Button variant="secondary" type="button" onClick={this.push_data}>
             submit
           </Button>
         </Form>

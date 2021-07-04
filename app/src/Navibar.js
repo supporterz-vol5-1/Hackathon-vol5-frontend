d0926955736b24e3e820ca4e2969985284b4935b
keimoriyama <keischwiiz@gmail.com>
2021-07-04 10:13:16 +0900

    boostrapを適用

--- app/src/Navibar.js
+++ app/src/Navibar.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import { NavDropdown, Nav } from "react-bootstrap";
 
 class Navibar extends React.Component {
   render() {
@@ -8,15 +11,28 @@ class Navibar extends React.Component {
 
 function RenderBarElement() {
   return (
-    <div>
-      <BarElement name="register" />
-      <BarElement name="graph" />
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand>コーディング時間</Navbar.Brand>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <DropdownMenu />
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-function BarElement(props) {
-  return <div>{props.name}</div>;
+function DropdownMenu() {
+  return (
+    <NavDropdown title="account menu" id="cnav-dropdown">
+      <DropdownElemnt name="register" />
+      <DropdownElemnt name="change account" />
+    </NavDropdown>
+  );
+}
+
+function DropdownElemnt(props) {
+  return <NavDropdown.Item>{props.name}</NavDropdown.Item>;
 }
 
 export default Navibar;
